4b55f6166288536ac239de7c2a8c82dc3efca111
Iwan <iwancuah13@gmail.com>
2021-06-18 13:51:23 +0700

    authentication implemented

---
+++ migrations/1623986328668_create-table-authentications.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+exports.up = pgm => {
+    pgm.createTable('authentications', {
+        token: {
+            type: 'TEXT',
+            notNull: true
+        }
+    })
+};
+
+exports.down = pgm => {
+    pgm.dropTable('authentications')
+};
