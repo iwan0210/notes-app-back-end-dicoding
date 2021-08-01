a5006261a83c507dd03acbf9f254845b82dc27dc
Iwan <iwancuah13@gmail.com>
2021-08-01 12:41:29 +0700

    collaboration implemented

---
+++ src/api/collaborations/routes.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+
+const routes = (handler) => [
+    {
+        method: 'POST',
+        path: '/collaborations',
+        handler: handler.postCollaborationHandler,
+        options: {
+            auth: 'notesapp_jwt'
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/collaborations',
+        handler: handler.deleteCollaborationHandler,
+        options: {
+            auth: 'notesapp_jwt'
+        }
+    }
+]
+
+module.exports = routes
--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -67,7 +67,7 @@ class NotesHandler {
             const { id } = request.params
             const { id: credentialId } = request.auth.credentials
 
-            await this._service.verifyNoteOwner(id, credentialId)
+            await this._service.verifyNoteAccess(id, credentialId)
             const note = await this._service.getNoteById(id)
             return {
                 status: 'success',
@@ -100,7 +100,7 @@ class NotesHandler {
             const { id } = request.params
             const { id: credentialId } = request.auth.credentials
 
-            await this._service.verifyNoteOwner(id, credentialId)
+            await this._service.verifyNoteAccess(id, credentialId)
             await this._service.editNoteById(id, request.payload)
 
             return {
@@ -156,6 +156,7 @@ class NotesHandler {
             return response;
         }
     }
+
 }
 
 module.exports = NotesHandler
