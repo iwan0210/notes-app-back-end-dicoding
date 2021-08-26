e851155bc2ae68ddf1056d870e2c12272f7182b6
Iwan <iwancuah13@gmail.com>
2021-08-26 19:45:35 +0700

    local storage implemented

---
+++ src/validator/uploads/schema.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+const Joi = require('joi')
+
+const ImageHeadersSchema = Joi.object({
+    'content-type': Joi.string().valid('image/apng', 'image/avif', 'image/gif', 'image/jpeg', 'image/png', 'image/webp').required()
+}).unknown()
+
+module.exports = { ImageHeadersSchema }
